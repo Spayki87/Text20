f396f8f8ecabcd32aff2896a572a9567cae36abc
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-07-27 14:49:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UlElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = 4;
+            int maxHealth = 10;
+
+            DrawBar(health, maxHealth, ConsoleColor.Green);
+        }
+        static void DrawBar(int value, int maxValue, ConsoleColor color)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+
+            string bar = "";
+
+            for (int i = 0; i < value; i++)
+            {
+                bar += "#";
+            }
+
+            Console.SetCursorPosition(0, 0);
+            Console.Write('[');
+            Console.ForegroundColor = color;
+            Console.Write(bar);
+            Console.ForegroundColor = defaultColor;
+
+            bar = "";
+
+            for (int i = value; i < maxValue; i++)
+            {
+                bar += "_";
+            }
+
+            Console.Write(bar + ']');
+            Console.WriteLine();
+        }
+    }
+}
