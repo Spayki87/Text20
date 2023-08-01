3b43c37f7fc902c8822eb616bfc3e8e266a1507d
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-08-01 23:25:52 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,37 +11,70 @@ namespace UlElement
     {
         static void Main(string[] args)
         {
-            int health = 4;
-            int maxHealth = 10;
+            int percentageHealth;
+            Console.WriteLine("Введите процент здоровья: ");
+            percentageHealth = Convert.ToInt32(Console.ReadLine());
 
-            DrawBar(health, maxHealth, ConsoleColor.Green);
+            int percentageMana;
+            Console.WriteLine("Введите процент маны: ");
+            percentageMana = Convert.ToInt32(Console.ReadLine());
+
+            int maxPercent = 100;
+
+            percentageMana = Clamp(percentageMana, 0, maxPercent);
+            percentageHealth = Clamp(percentageHealth, 0, maxPercent);
+
+            int healthBar = 20;
+            int currentHealth = healthBar * percentageHealth / maxPercent;
+            int manaBar = 20;
+            int currentMana = manaBar * percentageMana / maxPercent;
+
+            Console.WriteLine("Ваше сдоровье:");
+
+            DrawBar(currentHealth, healthBar, ConsoleColor.Green, 5);
+            DrawBar(currentMana, manaBar, ConsoleColor.Blue, 6);
         }
-        static void DrawBar(int value, int maxValue, ConsoleColor color)
-        {
-            ConsoleColor defaultColor = Console.ForegroundColor;
 
-            string bar = "";
+        private static int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+            {
+                value = min;
+            }
+
+            if (value > max)
+            {
+                value = max;
+            }
+
+            return value;
+        }
+
+        static void DrawBar(int value, int maxValue, ConsoleColor color, int position)
+        {
+            ConsoleColor defaultColor = Console.BackgroundColor;
+
+            string filledBar = "";
 
             for (int i = 0; i < value; i++)
             {
-                bar += "#";
+                filledBar += "#";
             }
 
-            Console.SetCursorPosition(0, 0);
-            Console.Write('[');
-            Console.ForegroundColor = color;
-            Console.Write(bar);
-            Console.ForegroundColor = defaultColor;
+            Console.SetCursorPosition(0, position);
+            Console.Write("[");
+            Console.BackgroundColor = color;
+            Console.Write(filledBar);
+            Console.BackgroundColor = defaultColor;
 
-            bar = "";
+            string emptyBar = "";
 
             for (int i = value; i < maxValue; i++)
             {
-                bar += "_";
+                emptyBar += '_';
             }
 
-            Console.Write(bar + ']');
-            Console.WriteLine();
+            Console.Write($"{emptyBar}]\n");
         }
     }
 }
